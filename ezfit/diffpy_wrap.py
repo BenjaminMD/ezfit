f54109dbf4cc5cd5bab950c7e6ebe719c6380804
Benjamin <Benjamin.Fahl@rwth-aachen.de>
2022-10-26 14:58:04 +0200

    switched to parrallel execution

--- ezfit/diffpy_wrap.py
+++ ezfit/diffpy_wrap.py
@@ -41,8 +41,8 @@ def _create_recipe(
     for name, crystal in crystals.items():
         pg = PDFGenerator(name)
         pg.setStructure(crystal, periodic=True)
-        # pg.parallel(32)
-        pg._calc.evaluatortype = 'OPTIMIZED'
+        pg.parallel(32)
+        #pg._calc.evaluatortype = 'OPTIMIZED'
         fc.addProfileGenerator(pg)
     if functions:
         for name, (f, argnames) in functions.items():
